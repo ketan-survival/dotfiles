7302e5fbf1db80c074ace6e24a0029ea90f5e9bb
Ketan Patel <ketpat@gmail.com>
2018-01-05 22:48:34 +0000

    Refactor scripts to use a genereic function
    This reduces repetetive code that symlinks and
    removes symlinks

--- install.sh
+++ install.sh
@@ -1,48 +1,30 @@
 #!/usr/bin/env bash
 
+function link_file() {
+  FILE=$1
+  if [ -e ~/$FILE ]; then
+    mv ~/$FILE "~/$FILE.bak"
+  fi
+  ln -s ~/.dotfiles/$FILE ~/$FILE
+}
+
 # install rvm and the latest ruby
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 # configure git
-GITCONFIG=~/.gitconfig
-if [ -e $GITCONFIG ]; then
-  mv $GITCONFIG "$GITCONFIG.bak"
-fi
-ln -s ~/.dotfiles/.gitconfig $GITCONFIG
-
-GITIGNORE_GLOBAL=~/.gitignore_global
-if [ -e $GITIGNORE_GLOBAL ]; then
-  mv $GITIGNORE_GLOBAL "$GITIGNORE_GLOBAL.bak"
-fi
-ln -s ~/.dotfiles/.gitconfig $GITIGNORE_GLOBAL
-
+link_file .gitconfig
+link_file .gitignore_global
 git clone https://github.com/jimeh/git-aware-prompt.git ~/.bash/git-aware-prompt
 
 # setup vim with plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-VIMRC=~/.vimrc
-if [ -e $VIMRC ]; then
-  mv $VIMRC "$VIMRC.bak"
-fi
-ln -s ~/.dotfiles/.vimrc $VIMRC
-
+link_file .vimrc
 vim -c 'PluginInstall' -c 'qa!'
 
-# symlink .inputrc and .bashrc
-INPUTRC=~/.inputrc
-if [ -e $INPUTRC ]; then
-  mv $INPUTRC "$INPUTRC.bak"
-fi
-ln -s ~/.dotfiles/.inputrc $INPUTRC
+link_file .inputrc
+link_file .bashrc
 
-BASHRC=~/.bashrc
-if [ -e $BASHRC ]; then
-  mv $BASHRC "$BASHRC.bak"
-fi
-ln -s ~/.dotfiles/.bashrc $BASHRC
-
-source $BASHRC
+source ~/.bashrc
 
 echo ">>> Install completed successfully, please restart all your sessions"
